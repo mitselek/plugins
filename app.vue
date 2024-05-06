2012f2a414dc639f6026c3b21e5ab7dcdf7ebdc3
Argo Roots <argo@roots.ee>
2024-05-06 09:15:04 +0300

    Add NaiveUI

--- app.vue
+++ app.vue
@@ -1,7 +1,28 @@
 <script setup>
+import { NConfigProvider, NGlobalStyle, enUS, etEE, dateEnUS, dateEtEE } from 'naive-ui'
 
+const { query } = useRoute()
+const currentLocale = ref(enUS)
+const currentDateLocale = ref(dateEnUS)
+
+onMounted(() => {
+  if (query.locale === 'et') {
+    currentLocale.value = etEE
+    currentDateLocale.value = dateEtEE
+  }
+})
 </script>
 
 <template>
-  <nuxt-page />
+  <n-config-provider
+    class="h-full"
+    inline-theme-disabled
+    :locale="currentLocale"
+    :date-locale="currentDateLocale"
+    :theme-overrides="themeOverrides"
+  >
+    <nuxt-page />
+
+    <n-global-style />
+  </n-config-provider>
 </template>
---
+++ utils/app.js
@@ -0,0 +1,73 @@
+export const themeOverrides = {
+  common: {
+    fontFamily: 'Avenir, "Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: '16px',
+    primaryColor: '#1E434C'
+  },
+  Collapse: {
+    dividerColor: '#FFFFFF'
+  },
+  DataTable: {
+    tdColorHover: '#FFFFFF',
+    thColor: '#FFFFFF',
+    thColorHover: '#FFFFFF'
+  },
+  Drawer: {
+    borderRadius: '0',
+    resizableTriggerColorHover: '#1E434C'
+  },
+  Input: {
+    borderFocus: '1px solid #1E434C',
+    borderHover: '1px solid #1E434C',
+    boxShadowFocus: '0 0 0 2px rgba(3, 117, 255, 0.2)'
+  },
+  Layout: {
+    siderColor: '#1E434C'
+  },
+  Menu: {
+    arrowColor: 'rgba(255, 255, 255, 0.7)',
+    arrowColorActive: '#FFFFFF',
+    arrowColorActiveHover: '#FFFFFF',
+    arrowColorChildActive: '#FFFFFF',
+    arrowColorChildActiveHover: '#FFFFFF',
+    arrowColorHover: 'rgba(255, 255, 255, 0.7)',
+
+    dividerColor: 'rgba(255, 255, 255, .2)',
+
+    itemColorActive: 'rgba(255, 255, 255, 0.15)',
+    itemColorActiveCollapsed: 'rgba(255, 255, 255, 0.15)',
+    itemColorActiveHover: 'rgba(255, 255, 255, 0.15)',
+    itemColorHover: 'rgba(255, 255, 255, 0.15)',
+
+    itemIconColor: 'rgba(255, 255, 255, 0.7)',
+    itemIconColorActive: '#FFFFFF',
+    itemIconColorActiveHover: '#FFFFFF',
+    itemIconColorChildActive: '#FFFFFF',
+    itemIconColorChildActiveHover: '#FFFFFF',
+    itemIconColorCollapsed: 'rgba(255, 255, 255, 0.7)',
+    itemIconColorCollapsedChildActive: '#FFFFFF',
+    itemIconColorHover: 'rgba(255, 255, 255, 0.7)',
+
+    itemTextColor: 'rgba(255, 255, 255, 0.7)',
+    itemTextColorActive: '#FFFFFF',
+    itemTextColorActiveHover: '#FFFFFF',
+    itemTextColorChildActive: '#FFFFFF',
+    itemTextColorChildActiveHover: 'rgba(255, 255, 255, .5)',
+    itemTextColorHover: 'rgba(255, 255, 255, 0.7)'
+  },
+  Select: {
+    menuBoxShadow: '0 0 0 1px rgb(3, 117, 255)',
+    peers: {
+      InternalSelection: {
+        borderFocus: '1px solid #1E434C',
+        borderHover: '1px solid #1E434C'
+      }
+    }
+  },
+  Slider: {
+    dotBorder: '2px solid #1E434C',
+    dotBorderActive: '2px solid #1E434C',
+    fillColor: '#DBDBDF',
+    fillColorHover: '#DBDBDF'
+  }
+}
