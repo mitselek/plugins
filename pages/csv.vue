bfbbbb5b1829caf213a45046aa0aa5f7993e705a
Argo Roots <argo@roots.ee>
2024-05-06 12:23:02 +0300

    Update csv.vue

--- pages/csv.vue
+++ pages/csv.vue
@@ -3,5 +3,25 @@
 </script>
 
 <template>
-  <h1>CSV</h1>
+  <div class="size-full pt-6 bg-green-100">
+    <h1 class="text-center">
+      CSV
+    </h1>
+
+    <p class="mb-6">
+      Voluptate Lorem deserunt esse tempor elit anim proident id proident fugiat et aute nostrud aute. Laboris enim sit eiusmod ea non non exercitation id incididunt adipisicing tempor magna adipisicing eu. In proident quis velit occaecat excepteur Lorem sit occaecat cupidatat do ullamco tempor esse esse. Laborum esse exercitation do Lorem aliqua amet. Ut proident id proident id est. Laborum ea officia eiusmod anim aute labore. Do proident veniam proident sint ea Lorem duis adipisicing quis voluptate occaecat laboris.
+    </p>
+    <p class="mb-6">
+      Incididunt adipisicing elit aute sit elit nisi aliquip incididunt nulla amet officia. Labore exercitation qui officia esse sit occaecat amet laboris laboris sunt eiusmod amet laboris. Ea eu in tempor eu consectetur Lorem ad quis sint commodo mollit veniam tempor aliqua.
+    </p>
+    <p class="mb-6">
+      Nisi cupidatat adipisicing nisi ut occaecat sint adipisicing exercitation aliquip dolor do mollit laboris. Incididunt aute ad esse amet deserunt commodo ad et duis id ea sunt. Sint mollit id cillum enim cupidatat laborum sint sit ex non do fugiat excepteur. Nisi magna sint eiusmod aliqua. Proident dolor incididunt minim officia excepteur velit aute do cupidatat laborum occaecat aute officia laboris. Commodo exercitation amet laborum eu incididunt proident quis nisi eu deserunt deserunt laboris anim consequat.
+    </p>
+    <p class="mb-6">
+      Enim cillum ipsum incididunt sunt enim excepteur id minim quis proident consectetur. Tempor enim est ullamco ea exercitation proident incididunt reprehenderit tempor proident elit. Adipisicing adipisicing nisi mollit consectetur consequat incididunt aliqua enim quis fugiat. Duis culpa mollit commodo fugiat esse mollit eu sint occa
+    </p>
+    <p class="mb-6">
+      Sint aliqua excepteur do voluptate voluptate veniam occaecat quis velit. Duis sunt aliquip officia sunt culpa dolor officia adipisicing ullamco elit ipsum mollit. Voluptate pariatur qui irure exercitation. Aliquip in pariatur deserunt amet reprehenderit velit ut id cupidatat cupidatat ad.
+    </p>
+  </div>
 </template>
