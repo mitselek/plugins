4fd58dd25709724226e3bd6fef426cd4f70a84aa
Argo Roots <argo@roots.ee>
2024-09-04 18:47:18 +0300

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-09-04',
   css: ['~/assets/tailwind.css'],
   devtools: { enabled: false },
   modules: [
